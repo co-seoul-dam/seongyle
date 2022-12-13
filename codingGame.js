7be25c84a9ab29c76d8b2526ba1a8ed2fc013883
yeonseong <yeonseong.dev@gmail.com>
2022-12-13 23:48:10 +0900

    FEAT: random walk

--- codingGame.js
+++ codingGame.js
@@ -2,10 +2,10 @@
 
 class Map
 {
-	constructor(height, width) {
+    constructor(height, width) {
 		this.height = height;
 		this.width = width;
-	}
+    }
 
 	getCenter() {
 		return [this.height/2,  this.width];
@@ -33,34 +33,62 @@ class Tile
 		this.canSpawn = canSpawn
 		this.inRangeOfRecycler = inRangeOfRecycler 
 	}
+
+	move(amount, toX, toY) {
+		process.stdout.write(`MOVE ${amount} ${this.x} ${this.y} ${toX} ${toY};`)
+	}
+
+	moveRandom() {
+		const step = 1;
+		const amount = 1; // maybe amount can be random.
+		const uniform_data = Math.random();
+		console.error(uniform_data)
+
+		if (this.units <= 0)
+			return ;
+		if (uniform_data < 0.25)
+			this.move(amount, this.x + step, this.y);
+		else if (uniform_data < 0.5)
+			this.move(amount, this.x , this.y + step);
+		else if (uniform_data < 0.75)
+			this.move(amount, this.x - step, this.y);
+		else if (uniform_data < 1)
+			this.move(amount, this.x , this.y - step);
+		else
+			console.error("can't move.");
+	}
 };
 
 
 var inputs = readline().split(' ');
 const width = parseInt(inputs[0]);
 const height = parseInt(inputs[1]);
-
-
 const map = new Map(width, height);
-let tiles = [];
 
 // game loop
 while (true) {
-	var inputs = readline().split(' ');
-	const myMatter = parseInt(inputs[0]);
-	const oppMatter = parseInt(inputs[1]);
-	for (let i = 0; i < height; i++) {
-		for (let j = 0; j < width; j++) {
-			var inputs = readline().split(' ');
-			const scrapAmount = parseInt(inputs[0]);
-			const owner = parseInt(inputs[1]); // 1 = me, 0 = foe, -1 = neutral
-			const units = parseInt(inputs[2]);
-			const recycler = parseInt(inputs[3]);
-			const canBuild = parseInt(inputs[4]);
-			const canSpawn = parseInt(inputs[5]);
-			const inRangeOfRecycler = parseInt(inputs[6]);
+	let tiles = [];
+    var inputs = readline().split(' ');
+    const myMatter = parseInt(inputs[0]);
+    const oppMatter = parseInt(inputs[1]);
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            var inputs = readline().split(' ');
+            const scrapAmount = parseInt(inputs[0]);
+            const owner = parseInt(inputs[1]); // 1 = me, 0 = foe, -1 = neutral
+            const units = parseInt(inputs[2]);
+            const recycler = parseInt(inputs[3]);
+            const canBuild = parseInt(inputs[4]);
+            const canSpawn = parseInt(inputs[5]);
+            const inRangeOfRecycler = parseInt(inputs[6]);
 			const tile = new Tile(j, i, scrapAmount, owner, units, recycler, canBuild, canSpawn, inRangeOfRecycler);
 			tiles.push(tile);
-		}
+        }
+    }
+	myTiles = tiles.filter(tile => tile.owner === 1)
+	myTiles.forEach( tile => {
+		tile.moveRandom();
 	}
+	)
+	process.stdout.write('\n');
 }
