0cb7e5541b2739c8305ab1185a94fb68d5a6ccb1
yeonseong <yeonseong.dev@gmail.com>
2022-12-13 20:45:23 +0900

    CHORE: indent

--- codingGame.js
+++ codingGame.js
@@ -2,10 +2,10 @@
 
 class Map
 {
-    constructor(height, width) {
+	constructor(height, width) {
 		this.height = height;
 		this.width = width;
-    }
+	}
 
 	getCenter() {
 		return [this.height/2,  this.width];
@@ -46,21 +46,21 @@ let tiles = [];
 
 // game loop
 while (true) {
-    var inputs = readline().split(' ');
-    const myMatter = parseInt(inputs[0]);
-    const oppMatter = parseInt(inputs[1]);
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            var inputs = readline().split(' ');
-            const scrapAmount = parseInt(inputs[0]);
-            const owner = parseInt(inputs[1]); // 1 = me, 0 = foe, -1 = neutral
-            const units = parseInt(inputs[2]);
-            const recycler = parseInt(inputs[3]);
-            const canBuild = parseInt(inputs[4]);
-            const canSpawn = parseInt(inputs[5]);
-            const inRangeOfRecycler = parseInt(inputs[6]);
+	var inputs = readline().split(' ');
+	const myMatter = parseInt(inputs[0]);
+	const oppMatter = parseInt(inputs[1]);
+	for (let i = 0; i < height; i++) {
+		for (let j = 0; j < width; j++) {
+			var inputs = readline().split(' ');
+			const scrapAmount = parseInt(inputs[0]);
+			const owner = parseInt(inputs[1]); // 1 = me, 0 = foe, -1 = neutral
+			const units = parseInt(inputs[2]);
+			const recycler = parseInt(inputs[3]);
+			const canBuild = parseInt(inputs[4]);
+			const canSpawn = parseInt(inputs[5]);
+			const inRangeOfRecycler = parseInt(inputs[6]);
 			const tile = new Tile(i, j, scrapAmount, owner, units, recycler, canBuild, canSpawn, inRangeOfRecycler);
 			tiles.push(tile);
-        }
-    }
+		}
+	}
 }
